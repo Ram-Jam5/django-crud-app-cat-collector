2b7e18834a3f4199890d02768d0d041725583e92
Ram-Jam5 <edgeillusive@gmail.com>
2024-08-19 10:28:21 -0500

    add toy model and update views for cat detail to show linked toys. Form ffor toys and full update delete available for toys.

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Cat
+from django.views.generic import ListView, DetailView
+
+from .models import Cat, Toy
 from .forms import FeedingForm
 # Create your views here.
 
@@ -19,8 +21,14 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
+    toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))  # Fetch all toys
     feeding_form = FeedingForm()
-    return render(request, 'cats/detail.html', {'cat': cat, 'feeding_form':feeding_form})
+    return render(request, 'cats/detail.html', {
+        'cat': cat,
+        'feeding_form': feeding_form,
+        'toys': toys_cat_doesnt_have  # Pass toys to the template
+    })
+
 
 
 def add_feeding(request, cat_id):
@@ -38,7 +46,8 @@ def add_feeding(request, cat_id):
 
 class CatCreate(CreateView):
     model = Cat
-    fields = '__all__'
+    fields = ['name', 'breed', 'description', 'age']
+
   
 class CatUpdate(UpdateView):
     model = Cat
@@ -47,3 +56,35 @@ class CatUpdate(UpdateView):
 class CatDelete(DeleteView):
     model = Cat
     success_url = '/cats/'
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
+
+def associate_toy(request, cat_id, toy_id):
+    # Note that you can pass a toy's id instead of the whole object
+    Cat.objects.get(id=cat_id).toys.add(toy_id)
+    return redirect('cat-detail', cat_id=cat_id)
+
+def remove_toy(request, cat_id, toy_id):
+    # Look up the cat
+    cat = Cat.objects.get(id=cat_id)
+    # Look up the toy 
+    cat.toys.remove(toy_id)
+    # toy = Toys.objects.get()
+    # Remove the toy from the cat
+    return redirect('cat-detail', cat_id=cat.id)
